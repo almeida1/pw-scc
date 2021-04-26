5e40a9e610bd8d7415223b8ee1e1adb19d368950
profe <profe@192.168.100.12>
2021-04-26 16:28:36 -0300

    Refatoração da funcao save ConstraintViolationException

--- scc/src/main/java/com/fatec/scc/servico/ClienteServicoI.java
+++ scc/src/main/java/com/fatec/scc/servico/ClienteServicoI.java
@@ -1,7 +1,10 @@
 package com.fatec.scc.servico;
 
+import javax.persistence.PersistenceException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,26 +12,31 @@ import org.springframework.web.servlet.ModelAndView;
 import com.fatec.scc.model.Cliente;
 import com.fatec.scc.model.ClienteRepository;
 import com.fatec.scc.model.Endereco;
+
 @Service
-public class ClienteServicoI implements ClienteServico{
+public class ClienteServicoI implements ClienteServico {
 	Logger logger = LogManager.getLogger(ClienteServicoI.class);
 	@Autowired
 	private ClienteRepository repository;
-	
+
 	public Iterable<Cliente> findAll() {
 		return repository.findAll();
 	}
+
 	public Cliente findByCpf(String cpf) {
 		return repository.findByCpf(cpf);
 	}
+
 	public void deleteById(Long id) {
 		repository.deleteById(id);
 		logger.info(">>>>>> 2. comando exclusao executado para o id => " + id);
 	}
+
 	public Cliente findById(Long id) {
 		return repository.findById(id).get();
 	}
-	public ModelAndView saveOrUpdate (Cliente cliente) {
+
+	public ModelAndView saveOrUpdate(Cliente cliente) {
 		ModelAndView modelAndView = new ModelAndView("consultarCliente");
 		try {
 			String endereco = obtemEndereco(cliente.getCep());
@@ -39,13 +47,19 @@ public class ClienteServicoI implements ClienteServico{
 				modelAndView.addObject("clientes", repository.findAll());
 			}
 
-		} catch (Exception e) { // captura validacoes na camada de persistencia
+		} catch (Exception e) {
 			modelAndView.setViewName("cadastrarCliente");
-			modelAndView.addObject("message", "Dados invalidos - " + e.getMessage());
-			logger.error(">>>>>> 4. erro nao esperado ==> " + e.getMessage());
+			if (e.getMessage().contains("could not execute statement")) {
+				modelAndView.addObject("message", "Dados invalidos - cliente já cadastrado.");
+				logger.info(">>>>>> 5. cliente ja cadastrado ==> " + e.getMessage());
+			} else {
+				modelAndView.addObject("message", "Erro não esperado - contate o administrador");
+				logger.error(">>>>>> 5. erro nao esperado ==> " + e.getMessage());
+			}
 		}
 		return modelAndView;
 	}
+
 	public String obtemEndereco(String cep) {
 		RestTemplate template = new RestTemplate();
 		String url = "https://viacep.com.br/ws/{cep}/json/";
@@ -53,5 +67,5 @@ public class ClienteServicoI implements ClienteServico{
 		logger.info(">>>>>> 3. obtem endereco ==> " + endereco.toString());
 		return endereco.getLogradouro();
 	}
-	
+
 }
